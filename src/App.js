79d8b95503b95e48d566486fdad29f4d60201ce2
amritansh-pandey <amritansh.p1010@gmail.com>
2022-07-04 22:24:38 +0530

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import List from "./pages/list/list";
 import Event from "./pages/event/event";
 import Checkout from "./pages/checkout/checkout";
+import Navbar from "./component/navbar/navbar";
+import Error404 from "./pages/error/error404";
 
 
 function App() {
   return (
-    <div>
-      <h1>Event Booking System</h1>
+    <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="list" element={<List />} />
         <Route path="event" element={<Event />} />
         <Route path="checkout" element={<Checkout />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
-      <Link to="/list"> List
-      </Link>
-    </div>
+    </Router>
   );
 }
 
--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,9 +1,17 @@
 import "./navbar.css";
 
 const Navbar = () => {
-return (
-    <div className="navbar">Navigation Bar</div>
-)
+    return (
+        <div className="navbar">
+            <div className="navContainer">
+                <span className="logo">Event Booking.com</span>
+                <input type="text" placeholder="Where are you going?" className="navSearchInput" />
+                <button className="navButton">Register</button>
+                <button className="navButton">Login</button>
+            </div>
+        </div>
+
+    )
 }
 
-export default Navbar;
+export default Navbar
