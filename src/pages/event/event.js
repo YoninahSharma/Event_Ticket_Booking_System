1c6f61731672c8adafa0a0231055c1b162a16acd
yoninah2000 <s.yoninah@gmail.com>
2022-07-15 23:49:29 +0530

    detailpageDone

--- src/pages/event/event.js
+++ src/pages/event/event.js
@@ -3,32 +3,36 @@ import "./event.css";
 const Event = () => {
   return (
     <div className="event">
-      <div className="eventBanner">
+      <div className="eventBannerContaine">
         <img
-          src="https://i.ibb.co/jhLf6sF/gaurav-banner.png"
+          src="https://i.ibb.co/D5Wm5Xr/gaurav-banner.png"
           alt="eventBanner"
+          className="eventBanner"
         />
       </div>
       <div className="eventContainer">
         <div className="eventDetail">
-          <div>
+          <div className="eventTitleInfo">
             <span className="eventTitle">Gaurav Kapoor Live</span>
             <span className="eventInfo">Comdey Show | Hindi | 2 hrs</span>
-          </div>
-          <div>
-            <span className="eventPrice">₹499</span>
           </div>
           <div>
             <button className="bookNow">Book Now</button>
           </div>
         </div>
+        <div className="eventDateTimePrice">
+          <span className="eventDate">17th July 2022 at 7 PM</span>
+          <span className="eventPlace">Maharana Pratap Auditorium: Noida</span>
+          <span className="eventPrice">₹499</span>
+        </div>
         <div className="eventLayout">
           <div className="artistInfo">
+            <span className="artist">Artist</span>
             <span className="artistName">Gaurav Kapoor</span>
             <div className="socialMedia">
-              <span>Facebook</span>
-              <span>Instagram</span>
-              <span>Youtube</span>
+              <span>F</span>
+              <span>I</span>
+              <span>Y</span>
             </div>
           </div>
           <div className="eventDescription">
@@ -39,7 +43,8 @@ const Event = () => {
               blues. You’ll have a good cry..laughing!"
             </span>
           </div>
-          <div className="eventLocation">
+          <div className="eventCityPlaceLocation">
+            <span className="city">Noida</span>
             <span className="place">Maharana Pratap Auditorium: Noida</span>
             <span className="location">
               Nilgiri Hills Public School, F-1, F Block, Sector 50, Noida, NCR
